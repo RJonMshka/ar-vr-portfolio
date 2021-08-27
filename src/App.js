abc5a76fbb01374f6ef751880ce4602eccdde59b
rajat <rkumar201@sapient.com>
2021-08-27 13:53:07 -0500

    Added UIN

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <img className="app-image" src="https://images.unsplash.com/photo-1554474054-1258273559a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2250&q=80" alt="bg-image" />
       <header className="app-header">
         <h1>Rajat Kumar</h1>
+        <p>UIN: 653922910</p>
       </header>
       <main className="main">
         <h2>CS 428 - Virtual, Augmented and Mixed Reality</h2>
