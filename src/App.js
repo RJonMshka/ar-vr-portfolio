c4ca296dbb5c199daa7c96bcd4682ec1d79f2df5
rajat <rkumar201@sapient.com>
2021-08-27 00:46:07 -0500

    Added HW1 content and image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>
+    <div className="app">
+      <img className="app-image" src="https://images.unsplash.com/photo-1554474054-1258273559a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2250&q=80" alt="bg-image" />
+      <header className="app-header">
         <h1>Rajat Kumar</h1>
-        <h2>My CS-428 Portfolio</h2>
       </header>
+      <main className="main">
+        <h2>CS 428 - Virtual, Augmented and Mixed Reality</h2>
+        <p>Exciting content coming soon...</p>
+      </main>
+      <footer></footer>
     </div>
   );
 }
